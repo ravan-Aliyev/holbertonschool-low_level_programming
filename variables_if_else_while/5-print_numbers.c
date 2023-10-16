b96d566aebec9a581701c57710cd0e8981a53ac8
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-16 03:43:10 -0700

    print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Prints if number is postive or negative.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+int num = 0;
+while (num < 10)
+{
+printf("%d", num);
+num++;
+};
+putchar('\n');
+return (0);
+}
