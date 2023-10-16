3c77dd584660aad6a49f92d60de5138754190d41
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-16 03:52:51 -0700

    reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *main - Prints if number is postive or negative.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 25; i >= 0; i--)
+{
+putchar('a' + i);
+};
+
+putchar('\n');
+return (0);
+}
