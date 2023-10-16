c4f691a080b992d938570beb885545d63581c83c
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-16 04:51:01 -0700

    print comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - Prints if number is postive or negative.
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+putchar('0' + i);
+i < 9 ? putchar(',') : putchar('$');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
