edf4e0e3372aa7ab2547c72cb8391bfc12966570
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-16 02:22:18 -0700

    last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -22,7 +22,7 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastdigit);
 }
 else
 {
-printf("Last digit of %d is %d and is grather than 5\n", n, lastdigit);
+printf("Last digit of %d is %d and is greater than 5\n", n, lastdigit);
 }
 return (0);
 }
