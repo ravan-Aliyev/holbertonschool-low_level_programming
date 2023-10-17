fb43b7ebbd5f0a6dcf03ee8783774255c5425b02
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-17 04:39:23 -0700

    times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *times_table - Prints time table.
+ */
+void times_table(void)
+{
+	int x;
+	int j;
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			int times = i * j;
+
+			if (times <= 9)
+			{
+				printf(" ");
+			}
+			else
+			{
+				printf("%d", times / 10);
+			}
+			printf("%d", times % 10);
+			printf(",");
+			printf(" ");
+		}
+		printf("\n");
+	}
+}
