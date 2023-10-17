c2aa11f90b543dd33c12494d1a9d74cf4a8c410b
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-17 04:58:01 -0700

    sum of two number

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *add - Check c is letter or not.
+ *@a: is number for sum.
+ *@b: is number for sum.
+ *
+ *Return: Sum of a and b.
+ */
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
