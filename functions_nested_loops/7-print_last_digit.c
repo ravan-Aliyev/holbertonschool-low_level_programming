469dc022cfa4d8e3f64aa7d7f72749e48331b7dd
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-17 02:44:56 -0700

    last digit

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *print_last_digit - Check c is letter or not.
+ *@n: is number for the check.
+ *
+ *Return: Last digit of n.
+ */
+int print_last_digit(int n)
+{
+	int y = n % 10;
+
+	return (y);
+}
