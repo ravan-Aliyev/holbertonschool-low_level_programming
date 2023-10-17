ffd69f92f43ec9f0c3901796e9815afc1d67e16e
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-17 02:24:19 -0700

    absolute value of intiger

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_isalpha - Check c is letter or not.
+ *@n: is number for the check.
+ *
+ *Return: Absolute value of n.
+ */
+int _abs(int n)
+{
+	int y = abs(n);
+
+	return (y);
+}
