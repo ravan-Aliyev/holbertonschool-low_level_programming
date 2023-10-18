efad5a7e5f36fd3bd52dcfc90f742b15e827788f
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-17 22:50:36 -0700

    multipile two number

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * _isupper - check character is upper or not.
+ *@a: is number.
+ *@b: is number
+ *
+ * Return: Multipiles of a and b
+ */
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	return (multi);
+}
