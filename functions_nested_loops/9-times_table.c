cdade4ffccc3019b8f348f614af133276fe0646d
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-18 06:06:40 -0700

    times table

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,30 +7,26 @@ void times_table(void)
 {
 	int i;
 	int j;
+	int times;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j < 10; j++)
+		_putchar('0');
+		for (j = 1; j < 10; j++)
 		{
-			int times = i * j;
+			_putchar(',');
+			_putchar(' ');
+			times = i * j;
 
 			if (times <= 9)
 			{
-				printf(" ");
+				_putchar(' ');
 			}
 			else
 			{
-				printf("%d", times / 10);
+				_putchar((times / 10) + '0');
 			}
-			if (times != 0)
-			{
-				printf("%d", times % 10);
-			}
-			if (j < 9)
-			{
-				printf(",");
-				printf(" ");
-			}
+			_putchar((times % 10) + '0');
 		}
 		printf("\n");
 	}
