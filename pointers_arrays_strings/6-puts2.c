a90f51edc93ff0158e84e93eefab23a9e3e93f74
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-23 04:04:38 -0700

    pointers

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * puts2 - print reverse value.
+ * @str: string.
+*/
+
+void puts2(char *str)
+{
+	size_t i;
+
+	for (i = 0; i < strlen(str) - 1; i += 2)
+	{
+		printf("%c", str[i]);
+	}
+}
