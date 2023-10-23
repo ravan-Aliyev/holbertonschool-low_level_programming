47472311c15cc863d2684ef9b314ac87eb6c05ab
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-23 04:21:30 -0700

    pointers

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ *puts_half - Prints half of string.
+ *@str: string.
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n = (strlen(str) - 1) / 2;
+	
+	for (i = 0; i <= n; i++)
+	{
+		printf("%c", str[i + n + 1]);
+	}
+	putchar('\n');
+}
