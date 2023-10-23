437d87be65b09b0dd22d143a4527ee54a55e2b02
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-23 05:14:20 -0700

    pointers

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcpy - print reverse value.
+ * @dest: string.
+ * @src: string.
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+
+	return (dest);
+}
