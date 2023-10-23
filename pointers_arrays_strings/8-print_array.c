a507881309ae1d9ee733cb15518d9742edb17fb1
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-23 04:56:41 -0700

    pointers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_array - print value of array.
+ * @a: array.
+ * @n: length of arr.
+*/
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	putchar('\n');
+}
