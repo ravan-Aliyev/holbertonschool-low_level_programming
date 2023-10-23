f4adc76859443f880f3522c8e1e03b1dd6c4144a
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-23 07:00:38 -0700

    pointers

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -10,7 +10,24 @@
 
 int _atoi(char *s)
 {
-	int num = atoi(s);
+	int sign = 1;
+	unsigned int num = 0;
 
-	return (num);
+	do {
+		if (*s == '-')
+		{
+			sign *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			num = (num * 10) + (*s - '0');
+		}
+		else if (num > 0)
+		{
+			break;
+		}
+
+	} while (*s++);
+
+	return (num * sign);
 }
