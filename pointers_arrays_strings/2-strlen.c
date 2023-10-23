f27ed15c9b8e68a682b1cd430e4b727b3a563c71
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-23 03:22:07 -0700

    pointers

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - Prints length of string.
+ * @s: string.
+ *
+ * Return: Length of string.
+*/
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
