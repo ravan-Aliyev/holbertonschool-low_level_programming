4235c0f86f8d427d3ac33baf065aa5848fc73a4c
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-23 05:50:22 -0700

    pointers

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib>
+/**
+ * _atoi - Turn string into the number.
+ * @s: string
+ *
+ * Return: Number.
+*/
+
+int _atoi(char *s)
+{
+	int num = atoi(s);
+
+	return (num);
+}
