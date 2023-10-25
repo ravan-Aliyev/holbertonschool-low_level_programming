37c5c60594438e5ce73ed30dacc8bdb550c7fd4b
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-24 22:27:59 -0700

    more pointers

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -7,8 +7,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
-	dest[strlen(dest)] = '\0';
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 
 	return (dest);
 }
