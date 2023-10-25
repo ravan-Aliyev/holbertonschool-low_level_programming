c8d954b97eade1a11383be45b921a2523a66587c
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-25 03:47:11 -0700

    more pointer

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -8,14 +8,12 @@
 void reverse_array(int *a, int n)
 {
 	int i;
+	int j = 0;
+	int copy[1000];
 
 	for (i = n - 1; i >= 0; i--)
 	{
-		printf("%d", a[i]);
-		if (i > 0)
-		{
-			printf(", ");
-		}
+		a[j] = copy[i];
+		j++;
 	}
-	putchar('\n');
 }
