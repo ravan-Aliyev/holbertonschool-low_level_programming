70e8956c744369d02b3bf9038182ef1158aa7f60
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-26 01:38:19 -0700

    more pointer

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+/**
+ *cap_string - capitalize string.
+ *@str: string.
+ *
+ * Return: Capitalized string.
+ */
+char *cap_string(char *str)
+{
+	int i;
+	int j;
+	char sepe[] = " ,.;!?\"({)}\n\t";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+		{
+			str[i] = str[i];
+		}
+		else
+		{
+			if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+			{
+				for (j = 0; j < sizeof(sepe); j++)
+				{
+					if (str[i - 1] == sepe[j])
+					{
+						str[i] == str[i] - 32;
+					}
+				}
+			}
+			else
+			{
+				str[i] = str[i];
+			}
+		}
+	}
+	str[i] = '\0';
+	return (str);
+}
