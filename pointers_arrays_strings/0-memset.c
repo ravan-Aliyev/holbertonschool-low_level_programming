b707b12a8ce2afcc339a331e6665185d56cd457f
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-27 03:22:47 -0700

    even more pointers

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * _memset - replace string.
+ * @s: string.
+ * @b: string what we changed to.
+ * @n: how many characters we changed.
+ *
+ * Return: changing character.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (s[i] != ' ')
+		{
+			s[i] = b;
+		}
+	}
+	return (s);
+}
