b0ad376c117b18552a2134050c4decdf9cdca0c6
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-27 03:55:00 -0700

    even more pointer

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (dest[i] != ' ')
+		if (dest[i] != ' ' || dest[i] != src[i])
 		{
 			dest[i] = src[i];
 		}
