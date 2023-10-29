9974d34b28d972bb89f9cc809024ddd93c01919b
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-29 02:05:09 -0700

    advenced tasks

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ *rot13 - Convert normal string to rot13.
+ *@s: string.
+ *
+ * Return: Rot13 string.
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char letter[] = "ABCDEFGHIJKLMNOPKRSTUVWXYZabcdefghijklmnopkrstuvwxyz";
+	char rot13letters[] = "NOPKRSTUVWXYZABCDEFGHIJKLMnopkrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == letter[j])
+			{
+				s[i] = rot13letters[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
