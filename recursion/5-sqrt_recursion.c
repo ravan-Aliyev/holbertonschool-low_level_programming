1b4a805d03490d5bd989d9af7f557067ff2cae69
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-30 03:58:47 -0700

    recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ *factorial - Find factorial of a number.
+ *@n: number.
+ *
+ *Return: -1 if n less than zero, 1 if n equal zero or one, otherwise factorial
+ */
+
+int find_sqrt(int num, int root)
+{
+	if ((root * root) == num)
+		return (root);
+	if (root == num / 2)
+		return (-1);
+
+	return (find_sqrt(n, root + 1));
+}
+
+int _sqrt_recursion(int n)
+{
+	int root = 0;
+
+	if (n < 0)
+		return (-1);
+	if (n == 1)
+		return (1);
+
+	return (find_sqrt(n, root));
+}
