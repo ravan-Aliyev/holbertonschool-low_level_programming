fe1592b75dfc3f136e9653d1b548427273ca5910
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-30 06:17:11 -0700

    what is my name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - print arguments
+ * @argc: size of argument.
+ * @argv: array of argument.
+ *
+ * Return: Succsess.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
