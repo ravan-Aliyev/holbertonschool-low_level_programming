c7d7742660fdfedfc39816b566411a6c80bd18d9
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-30 00:58:02 -0700

    recursion

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * _strlen_recursion - Find length of string.
+ * @s: string.
+ *
+ * Return: Length of string.
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (*s == '\0')
+	{
+		return;
+	}
+
+	s++;
+	i++;
+	return (i + _strlen_recursion);
+}
