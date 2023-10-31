c866cfe4f7f63b18a9925dd1a078c3ef098bd0e3
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-10-31 04:26:17 -0700

    adding arguments

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
  * main - Prints multiple of two arguments.
  * @argc: Count of arguments.
@@ -11,23 +12,24 @@ int main(int argc, char *argv[])
 {
 	int i;
 	int mul = 0;
+	size_t j;
 
 	if (argv[1] == NULL || argv[2] == NULL)
 	{
 		printf("0\n");
-		return (1);
+		return (0);
 	}
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]) == 0)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (atoi(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (0);
+			}
 		}
-		else
-		{
-			mul += atoi(argv[i]);	
-		}
+		mul += atoi(argv[i]);
 	}
 	printf("%d\n", mul);
 	return (0);
