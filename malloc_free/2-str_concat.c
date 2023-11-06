c797e64f1bd04368a54b287dec0424f4ff5f4aec
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-11-06 07:48:00 -0800

    malloc free

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,7 +18,7 @@ char *str_concat(char *s1, char *s2)
 	if (s2 == NULL)
 		s2 = "";
 
-	arr = (char *)malloc(strlen(s1) + strlen(s2) + 2);
+	arr = (char *)malloc(strlen(s1) + strlen(s2) + 1);
 
 	if (arr == NULL)
 	{
