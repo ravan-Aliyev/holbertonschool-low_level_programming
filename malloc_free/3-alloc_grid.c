741ecc09c9b1632272d78c8f2dbedca9c752c82b
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-11-06 08:29:26 -0800

    malloc free

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = (int *)malloc(sizeof(int **) * width);
+		arr[i] = (int *)malloc(sizeof(int) * width);
 
 		for (j = 0; j < width; j++)
 		{
