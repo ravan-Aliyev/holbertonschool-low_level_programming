9bb964ce23c70cd2efc1809a228bbf7da0ca7a54
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-11-06 07:47:09 -0800

    malloc free

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+/**
+ * str_concat - Concatinate string.
+ * @s1: string.
+ * @s2: string.
+ *
+ * Return: String.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *arr;
+	int i;
+	int index = 0;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	arr = (char *)malloc(strlen(s1) + strlen(s2) + 2);
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		arr[i] = s1[i];
+		index++;
+	}
+
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		arr[index] = s2[i];
+		index++;
+	}
+
+	return (arr);
+
+}
