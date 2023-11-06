5d33ec255be0dde658298ae080f7ab7fdd3468ff
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-11-06 08:55:14 -0800

    malloc free

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+/**
+ * free_grid - Free memory allocation.
+ * @grid: array.
+ * @height: size of row.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
