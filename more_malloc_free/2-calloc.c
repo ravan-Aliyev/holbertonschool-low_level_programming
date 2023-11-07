fbb5b5505c66d7d025144e75605fdbb740bcf5bf
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-11-07 05:53:50 -0800

    more malloc free

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -10,10 +10,21 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *arr = malloc(nmemb * size);
+	unsigned int i, j;
 
 	if (arr == NULL || nmemb == 0 || size == 0)
 	{
 		return (NULL);
+	}
+
+	for (i = 0; i < nmemb; i++)
+	{
+		unsigned char *element = (unsigned char *)arr + (i * size);
+
+		for (j = 0; j < size; j++)
+		{
+			element[j] = 0;
+		}
 	}
 
 	return (arr);
