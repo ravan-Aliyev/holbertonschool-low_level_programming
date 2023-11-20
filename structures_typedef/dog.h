b751a42e19fe3acc958c14f686f910a81297b398
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-11-20 04:50:49 -0800

    struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_HEADER
 #define DOG_HEADER
 
+/**
+ * struct dog - Contain name, age and owner member.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: name of the owner.
+ */
+
 struct dog
 {
 	char *name;
