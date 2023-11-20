3e419b61118ddd5b1b32801da12516a9dfe0a19b
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-11-20 07:16:48 -0800

    struct

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -2,18 +2,23 @@
 #include <stdlib.h>
 #include "dog.h"
 
-/** 
+/**
  * new_dog - create new dog.
  * @name: name of the dog.
  * @age: age of the dog.
  * @owner: name of the owner.
+ *
+ * Return: New dog struct.
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog = malloc(sizeof(dog_t));
 
 	if (my_dog == NULL)
+	{
+		free(my_dog);
 		return (NULL);
+	}
 
 	my_dog->name = name;
 	my_dog->age = age;
