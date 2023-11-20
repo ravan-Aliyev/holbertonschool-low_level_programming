5301c7656aeac98d60c26ef5534fbd6b3d373658
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-11-20 06:19:59 -0800

    struct

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -18,18 +18,5 @@ struct dog
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
-/**
- * struct dog_t - Contain name, age and owner member.
- * @name: name of the dog.
- * @age: age of the dog.
- * @owner: name of the owner.
- */
-
-typedef struct dog_t dog_t;
-struct dog_t
-{
-	char *name;
-	float age;
-	char *owner;
-};
+typedef struct dog dog_t;
 #endif
