95d628ccfedf7272641c03c0e3f74638e7a2e290
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-11-21 04:37:16 -0800

    struct

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - Free all allocated memory
+ * @d: struct dog.
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
