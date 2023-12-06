5eeefe9ad299532f967a251b8c33eb5b1cc92b09
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-06 05:44:17 -0800

    double linked list

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+#include <stddef.h>
+#include <stdio.h>
+/**
+ * print_dlistint - Prints all eleemnts of list.
+ * @h: head of list.
+ *
+ * Return: number of nodes.
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *curr;
+	size_t s = 0;
+
+	for (curr = h; curr != NULL; curr = curr->next)
+	{
+		printf("%d\n", curr->n);
+		s++;
+	}
+	/*for (curr = h; curr != NULL; curr = curr->prev)
+	{
+		printf("%d\n", curr->n);
+		s++;
+	}*/
+
+	return (s);
+}
