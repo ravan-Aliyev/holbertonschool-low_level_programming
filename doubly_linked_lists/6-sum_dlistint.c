95dc4b633a1dce216ef2f866fbb099fcec8c9a8d
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-08 00:25:47 -0800

    doble linked list

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * sum_dlistint - find sum of list elements
+ * @head: head of the list
+ *
+ * Return: Sum of the elements.
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	unsigned int sum;
+	
+	if (head != NULL)
+	{
+		while (head)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+		return (sum);
+	}
+
+	return (0);
+}
