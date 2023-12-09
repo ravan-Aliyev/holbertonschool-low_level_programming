509884b3478010c81d92303a2295f35442a31ebb
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-09 05:53:51 -0800

    bit manipulation

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -8,22 +8,10 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int i;
-	unsigned long int d = *n;
-
 	if (index > 32)
 		return (-1);
 
-	for (i = 0; i < index + 1; i++)
-		d >>= 1;
-
-	for (i = 0; i < index + 1; i++)
-		d <<= 1;
-
-	if (*n & 1)
-		*n |= d;
-	else
-		*n &= d;
+	*n &= ~(1 << index);	
 
 	return (1);
 }
