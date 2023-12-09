aaa13d8bfd6c455e56a517a25f1c91dd8f3ea1c2
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-09 05:33:46 -0800

    bit manipulation

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * clear_bit - setting 0 bit to the index.
+ * @n: number.
+ * @index: index.
+ *
+ * Return: 1 success -1 fail.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i;
+	unsigned long int d = *n;
+
+	if (index > 32)
+		return (-1);
+
+	for (i = 0; i < index + 1; i++)
+		d >>= 1;
+
+	for (i = 0; i < index + 1; i++)
+		d <<= 1;
+
+	*n = d & *n;
+
+	return (1);
+}
