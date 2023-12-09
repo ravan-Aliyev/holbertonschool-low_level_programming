97338517f161f89e84329ea25b04357c1cdf40d1
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-09 03:36:41 -0800

    bit manupilation

---
+++ bit_manipulation/1-print_binary.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * print_binary - Print binary numbers.
+ * @n: binary number.
+ */
+void print_binary(unsigned long int n)
+{
+	int result = 0;
+
+	if (n > 1)
+		print_binary(n >> 1);
+
+	result = n & 1;
+	return (result);
+}
