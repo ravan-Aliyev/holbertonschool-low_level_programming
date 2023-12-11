3a499d3e234395c7ee0a143456b7636ec4148ec4
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-11 05:44:12 -0800

    hash table

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hash_tables.h"
+/**
+ * hash_table_print - print hash table.
+ * @ht: hash table.
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	unsigned long int count = 0;
+
+	if (ht == NULL)
+		exit(98);
+
+	putchar('{');
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			if (count > 0)
+				printf(", ");
+			printf("'%s': '%s'", ht->array[i]->key, ht->array[i]->value);
+			count++;
+		}
+
+	}
+	putchar('}');
+	putchar('\n');
+}
