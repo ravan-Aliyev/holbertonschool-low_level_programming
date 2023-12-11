ae178270ef4ec37512a0eab7b067a6d51170d078
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-11 04:50:37 -0800

    hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,7 +16,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new = malloc(sizeof(hash_node_t));
 
 	if (key == NULL || value == NULL || new == NULL || ht == NULL)
-		return (-1);
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	new->key = strdup(key);
