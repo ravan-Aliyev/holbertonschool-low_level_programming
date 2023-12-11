de64934ab87aed79428fb76e5eb39cf7c9be6920
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-11 02:28:01 -0800

    create hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -8,10 +8,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash = malloc(sizeof(hash_table_t) * size);
+	hash_table_t *hash = malloc(sizeof(hash_table_t));
+	hash_node_t *arr = malloc(sizeof(hash_node_t) * size);
 
-	if (!hash)
+	if (!hash || !arr)
 		return (NULL);
+
+	hash->size = size;
+	hash->array = &arr; 
 
 	return (hash);
 }
