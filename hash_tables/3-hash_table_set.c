fabcba203c7992f6618799e0aa041a2066439c27
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-11 04:49:37 -0800

    hash table

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -12,17 +12,31 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
 	hash_node_t *new = malloc(sizeof(hash_node_t));
 
 	if (key == NULL || value == NULL || new == NULL || ht == NULL)
 		return (-1);
 
+	index = key_index((const unsigned char *)key, ht->size);
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
 
-	ht->array[index] = new;
+	if (ht->array[index] == NULL)
+	{
+		ht->array[index] = new;
+	}
+	else
+	{
+		if (strcmp(ht->array[index]->key, new->key) == 0)
+			ht->array[index]->value = strdup(value);
+		else
+		{
+			new->next = ht->array[index];
+			ht->array[index] = new;
+		}
+	}
 
 	return (1);
 }
