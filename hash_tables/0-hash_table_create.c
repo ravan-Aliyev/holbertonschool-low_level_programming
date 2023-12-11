be28055facd5d1153d27cafd63f83ed09d74c014
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-11 02:06:44 -0800

    create hash table

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+/**
+ * hash_table_create - Create hash table.
+ * @size: size of hash table.
+ *
+ * Return: Pointer to new hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash = malloc(sizeof(hash_node_t) * size);
+
+	if (!hash)
+		return (NULL);
+
+	return (hash);
+}
