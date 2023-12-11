a5b7129242da512b3759be714dc90069899c6c5f
ravan-Aliyev <132194049+ravan-Aliyev@users.noreply.github.com>
2023-12-11 14:46:11 +0400

    create  hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,7 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash = malloc(sizeof(hash_table_t));
-	hash_node_t *arr = malloc(size);
+	hash_node_t *arr = malloc(sizeof(hash_node_t *) * size);
 
 	if (!hash || !arr)
 		return (NULL);
