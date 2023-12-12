62a49d82cfa0ff026a23c4a48ea357a1f677862d
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-12 04:55:33 -0800

    file io

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdlib.h>
+/**
+ * read_textfile - Read text file.
+ * @filename: name of the file.
+ * @letters: letters.
+ *
+ * Return: Actual number of letters.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, n, w;
+	char *buf = malloc(sizeof(char) * letters + 1);
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd < 0)
+		return (0);
+
+	n = read(fd, buf, letters);
+
+	w = write(1, buf, n);
+
+	if (fd < 0 || n < 0 || w < 0)
+	{
+		free(buf);
+	}
+
+	free(buf);
+	close(fd);
+
+	return(w);
+}
