eb190dc38434ed709eb774a2df2a18315facba07
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-13 04:54:43 -0800

    file io

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -15,7 +15,6 @@ int main(int ac, char **av)
 {
 	int open_0, open_1, r, w, cl_0, cl_1;
 	char buf[1024];
-	mode_t file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (ac != 3)
 	{
@@ -24,28 +23,29 @@ int main(int ac, char **av)
 	}
 	open_0 = open(av[1], O_RDONLY);
 	r = read(open_0, buf, 1024);
-	if (open_0 < 0 || r < 0)
+	if (open_0 == -1 || r == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
 	cl_0 = close(open_0);
-	if (cl_0 < 0)
+	if (cl_0 == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close %d", open_0);
 		exit(100);
 	}
-	open_1 = open(av[2], O_CREAT | O_RDWR | O_TRUNC, file_perm);
+	open_1 = open(av[2], O_CREAT, O_RDWR, O_TRUNC, 0664);
 	w = write(open_1, buf, 1024);
-	if (open_1 < 0 || w < 0)
+	if (open_1 == -1 || w == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
 	cl_1 = close(open_1);
-	if (cl_1 < 0)
+	cl_0 = close(open_0);
+	if (cl_1 == -1 || cl_0 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close %d", open_0);
+		dprintf(STDERR_FILENO, "Error: Can't close fd");
 		exit(100);
 	}
 	return (0);
