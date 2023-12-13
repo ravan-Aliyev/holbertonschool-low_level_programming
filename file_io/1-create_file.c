7d1cabcd8b4949aa30e4c41c4d8ace1029a68e46
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-13 01:27:56 -0800

    file io

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -4,6 +4,7 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 /**
  * create_file - creating file.
  * @filename: name of the file.
@@ -27,11 +28,11 @@ int create_file(const char *filename, char *text_content)
 		}
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	o = open(filename, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR);
 	w = write(o, text_content, len);
 
-
-	if (o < 1 || w < 1)
+printf("%d, %d\n", w, o);
+	if (o == -1 || w == -1)
 	{
 		return (-1);
 	}
