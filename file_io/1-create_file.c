83438f36ba1c78632ed5c7f84e04bc1b957d510e
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-12 22:20:24 -0800

    file io

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,25 +14,18 @@
 int create_file(const char *filename, char *text_content)
 {
 	int o, w;
-	char *buf = malloc(strlen(text_content) + 1);
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
-	o = open(filename, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR);
-	w = write(STDOUT_FILENO, buf, sizeof(buf));
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	w = write(o, text_content, sizeof(text_content) + 1);
 
 
 	if (o < 1 || w < 1)
 	{
-		free(buf);
 		return (-1);
 	}
-
-	free(buf);
 	close(o);
 
 	return (1);
