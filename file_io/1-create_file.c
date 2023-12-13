209e388e61433dbdfb8ec9506affee7bc2d563c9
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-12 23:56:23 -0800

    file io

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,13 +14,19 @@
 int create_file(const char *filename, char *text_content)
 {
 	int o, w;
-	size_t len = 0;
+	int len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
-		len = strlen(text_content);
+	{
+		while (*text_content)
+		{
+			len++;
+			*text_content++;
+		}
+	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
 	w = write(o, text_content, len);
