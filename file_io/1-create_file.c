4f803a94a8ef2276ddc4047af85c9ed5a40af8b4
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-13 01:17:17 -0800

    file io

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -21,10 +21,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (*text_content)
+		while (text_content[len])
 		{
 			len++;
-			text_content++;
 		}
 	}
 
