bead69a2cd74d4ded5891fb689e5b6d31023fe97
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-16 22:46:20 -0800

    file io

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -68,7 +68,7 @@ int main(int ac, char **av)
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", av[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 			free(buffer);
 			exit(98);
 		}
