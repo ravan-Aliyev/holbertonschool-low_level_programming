343c684f56b2b977ec2e6ae070d819f1e73602fe
ravan-Aliyev <eliyevrevan18@outlook.com>
2023-12-16 22:41:54 -0800

    file io

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -54,21 +54,21 @@ int main(int ac, char **av)
 	int from, to, r, w;
 	char *buffer;
 
-	if (argc != 3)
+	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	buffer = create_buffer(argv[2]);
-	from = open(argv[1], O_RDONLY);
+	buffer = create_buffer(av[2]);
+	from = open(av[1], O_RDONLY);
 	r = read(from, buffer, 1024);
-	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(2, "Error: Can't read from file %s\n", av[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -76,13 +76,13 @@ int main(int ac, char **av)
 		w = write(to, buffer, r);
 		if (to == -1 || w == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", av[2]);
 			free(buffer);
 			exit(99);
 		}
 
 		r = read(from, buffer, 1024);
-		to = open(argv[2], O_WRONLY | O_APPEND);
+		to = open(av[2], O_WRONLY | O_APPEND);
 
 	} while (r > 0);
 
