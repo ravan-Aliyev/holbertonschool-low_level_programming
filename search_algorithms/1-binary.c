f5c2dcd734abac6dbf4869cfb75209255576dd1b
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-16 05:32:37 -0800

    binary serach

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * binary_search - serch value with linear search algorithm.
+ * @array: array.
+ * @size: size of array.
+ * @value: value for search.
+ *
+ * Return: idex of thr value.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	int low, high, mid, j;
+
+	if (array == NULL)
+		return (-1);
+
+	low = 0;
+	high = size - 1;
+	
+	while (low <= high)
+	{
+		mid = (low + high) / 2;
+		printf("Searching in array: ");
+
+		for (j = low; j <= high; j++)
+		{
+			printf("%d", array[j]);
+			if (j < high)
+				printf(", ");
+		}
+		printf("\n");
+		if (array[mid] == value)
+			return (mid);
+		else if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+	}
+
+	return (-1);
+}
