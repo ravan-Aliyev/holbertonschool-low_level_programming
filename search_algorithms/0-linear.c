2355f57a01dd8e55f663f02d4b39e75efefca31a
ravan-Aliyev <eliyevrevan18@outlook.com>
2024-01-16 05:07:29 -0800

    linear search

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,6 +11,9 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
 
+	if (array == NULL)
+		return (-1);
+
 	for (i = 0; i <= size - 1; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
